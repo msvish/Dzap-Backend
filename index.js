04c1faa05aba20efb5553e4ad5e07c1d54d86e18
Vishal M S <vishms1999@gmail.com>
2024-01-01 18:19:11 +0530

    feat: backend for Dzap

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+require("dotenv").config();
+
+const corsOptions = {
+  origin: ["*"],
+};
+
+app.use(cors(corsOptions));
+
+app.use(cors());
+
+app.get("/latestcoins", async (req, res) => {
+  try {
+    await fetch(process.env.GET_LISTS, {
+      headers: {
+        "X-CMC_PRO_API_KEY": process.env.API_KEY,
+      },
+      method: "GET",
+      params: {
+        sort: "market_cap",
+        limit: "100",
+        sort: "desc",
+      },
+    }).then(async (response) => {
+      if (response.ok) {
+        const data = await response.json();
+        let customResponse = [];
+        const coinData = data.data;
+        coinData.map((coin) => {
+          let obj = {
+            id: coin.id,
+            name: coin.name,
+          };
+          customResponse.push(obj);
+        });
+        res.status(200).json({ data: customResponse });
+      } else {
+        res.status(400).json({ message: `Https error ${response.status}` });
+      }
+    });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+app.get("/currencies", async (req, res) => {
+  try {
+    await fetch(process.env.GET_CURRIENCES, {
+      headers: {
+        "X-CMC_PRO_API_KEY": process.env.API_KEY,
+      },
+      method: "GET",
+      params: {
+        sort: "name",
+      },
+    }).then(async (response) => {
+      if (response.ok) {
+        const data = await response.json();
+        let customResponse = [];
+        const coinData = data.data;
+        coinData.map((coin) => {
+          let obj = {
+            id: coin.id,
+            name: coin.name,
+          };
+          customResponse.push(obj);
+        });
+        res.status(200).json({ data: customResponse });
+      } else {
+        res.status(400).json({ message: `Https error ${response.status}` });
+      }
+    });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+app.listen(3001, () => {
+  console.log("server listening at 3001");
+});
