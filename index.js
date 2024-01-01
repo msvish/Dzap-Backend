6d902e371d280e85bd5fc617b9f59f17e9d7090b
Vishal M S <vishms1999@gmail.com>
2024-01-02 00:03:22 +0530

    feat: created a conversion end point

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 require("dotenv").config();
 
 const corsOptions = {
-  origin: ["*"],
+  origin: "*",
 };
 
 app.use(express.json());
@@ -64,6 +64,7 @@ app.get("/currencies", async (req, res) => {
           let obj = {
             id: coin.id,
             name: coin.name,
+            sign: coin.sign,
           };
           customResponse.push(obj);
         });
@@ -79,39 +80,38 @@ app.get("/currencies", async (req, res) => {
 
 app.post("/conversion", async (req, res) => {
   const request = req.body;
-  console.log(request);
-  res.status(200).json({});
-  // try {
-  //   await fetch(process.env.GET_LISTS, {
-  //     headers: {
-  //       "X-CMC_PRO_API_KEY": process.env.API_KEY,
-  //     },
-  //     method: "GET",
-  //     params: {
-  //       sort: "market_cap",
-  //       limit: "100",
-  //       sort: "desc",
-  //     },
-  //   }).then(async (response) => {
-  //     if (response.ok) {
-  //       const data = await response.json();
-  //       let customResponse = [];
-  //       const coinData = data.data;
-  //       coinData.map((coin) => {
-  //         let obj = {
-  //           id: coin.id,
-  //           name: coin.name,
-  //         };
-  //         customResponse.push(obj);
-  //       });
-  //       res.status(200).json({ data: customResponse });
-  //     } else {
-  //       res.status(400).json({ message: `Https error ${response.status}` });
-  //     }
-  //   });
-  // } catch (error) {
-  // res.status(400).json({ message: error.message });
-  // }
+  try {
+    await fetch(
+      `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=100&convert_id=${request.currency}`,
+      {
+        headers: {
+          "X-CMC_PRO_API_KEY": process.env.API_KEY,
+        },
+        method: "GET",
+      }
+    ).then(async (response) => {
+      if (response.ok) {
+        const data = await response.json();
+        const coinData = data.data;
+        coinData.filter((coin) => {
+          if (coin.id === request.cryptocurrency) {
+            res
+              .status(200)
+              .send(
+                `Total price : ${request.sign}${
+                  coin["quote"][`${request.currency}`].price * request.amount
+                }`
+              );
+            return;
+          }
+        });
+      } else {
+        res.status(400).json({ message: `Https error ${response.status}` });
+      }
+    });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 app.listen(3001, () => {
